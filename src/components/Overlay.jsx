7e7187599fc6ad3b6a877e1b20178c8d66b497b3
rafael merlin <rafaelmerlinprieto@gmail.com>
2024-08-28 19:24:57 -0600

    terminando overlays falta boton de cancelar en la esquina de un contenedor

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -5,16 +5,16 @@ const OverlayElminar =({children}) => {
   
   return (
     <>
-        <div>
-          
-          <div className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
-              <div className=' bg-gray-700 p-5 rounded flex flex-col justify-center items-center gap-5'>
-                {children}
-              </div>
-          </div>
+    <div className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center mb-0 mt-0'>
+      <div className='relative bg-gray-700 px-5 pb-5 pt-5 rounded flex flex-col justify-center items-center gap-5 mb-0 mt-0'>
+        {/* Botón para cerrar */}
+        <button className='absolute top-2 left-2 text-white text-xl '>
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
 
-        </div>
-        
     </>
   )
 }
