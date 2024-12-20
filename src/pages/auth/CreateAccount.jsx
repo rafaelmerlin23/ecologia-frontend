fffc63c42f75a5198bad301d82028694bdfecb09
rafael <rafaelmerlinprieto@gmail.com>
2024-12-20 13:50:56 -0600

    cambios en el formulario de registro

--- src/pages/auth/CreateAccount.jsx
+++ src/pages/auth/CreateAccount.jsx
@@ -61,7 +61,7 @@ export const CreateAccount = () => {
                 
                 <div>
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tu correo</label>
-                    <input required value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="correo" id="correo" placeholder="negronAdrian@gmail.uv.mx" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                    <input required value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="correo" id="correo" placeholder="blanconAdrian@gmail.uv.mx" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 </div>
 
                 <div>
